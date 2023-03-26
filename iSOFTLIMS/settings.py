64abd29431250775abbfce887481b7ac518d697c
agarjoshua <joshuaagarjj@gmail.com>
2023-03-26 21:15:01 +0300

    this is to edit the bugs

--- iSOFTLIMS/settings.py
+++ iSOFTLIMS/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +26,9 @@ SECRET_KEY = "django-insecure-@2&whl+@3k8+rcsbt*0d+inj^gzz5ts8qt-+lnhnmu3k-11sk*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["isoft.azurewebsites.net","https://isoft.azurewebsites.net","*"]
+ALLOWED_HOSTS = ["*","isoft.azurewebsites.net","https://isoft.azurewebsites.net"]
 
+CSRF_TRUSTED_ORIGINS = ["https://isoft.azurewebsites.net/","https://*.127.0.0.1"]
 
 # Application definition
 
@@ -121,7 +123,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
