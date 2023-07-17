2373045ef817fbdb1039b5ef1a777a51c45a804c
agarjoshua <joshuaagarjj@gmail.com>
2023-07-17 10:06:11 +0300

    fixed the login screens

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from academics.subviews import classView, studentView
 from . import views
 from core.subviews import AdminViews, StudentViews,StudentAdminAffairs, ApplicantViews
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
@@ -12,7 +13,17 @@ urlpatterns = [
     path("register/", views.applicantloginPage, name="register"),
     path("doSignUp/", ApplicantViews.applicantsignup, name="doSignUp"), # type: ignore
 
-    path('', include('django.contrib.auth.urls')),
+    # path('', include('django.contrib.auth.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
+         name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
+         name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
 
     # path('reset/', views.PasswordResetView.as_view(), name='password_reset'),
     # # path('reset/', views.send_reset_email, name='password_reset'),
